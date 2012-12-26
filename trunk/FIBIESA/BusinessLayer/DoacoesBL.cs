b1eb40dfbb8c2a0b63d53fae10e40a3b2f011177
viniciusbalbinot91@gmail.com <viniciusbalbinot91@gmail.com@b50c96da-cf07-9db0-1c6e-8329b04c20b5>
2012-12-26 17:10:31 +0000

    Criado classes das tabela Doaçoes

---
+++ trunk/FIBIESA/BusinessLayer/DoacoesBL.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataObjects;
+using DataAccess;
+
+namespace BusinessLayer
+{
+    public class DoacoesBL
+    {
+        public bool InserirBL(Doacoes doa)
+        {
+            /*criar as regras de negocio*/
+            DoacoesDA doacoesDA = new DoacoesDA();
+
+            return doacoesDA.InserirDA(doa);
+        }
+
+        public bool EditarBL(Doacoes doa)
+        {
+            /*criar as regras de negocio*/
+            DoacoesDA doacoesDA = new DoacoesDA();
+
+            return doacoesDA.InserirDA(doa);
+        }
+
+        public bool ExcluirBL(Doacoes doa)
+        {
+            /*criar as regras de negocio*/
+            DoacoesDA doacoesDA = new DoacoesDA();
+
+            return doacoesDA.ExcluirDA(doa);
+        }
+
+
+        public List<Doacoes> PesquisarBL()
+        {
+            /*criar as regras de negocio*/
+            DoacoesDA doacoesDA = new DoacoesDA();
+
+            return doacoesDA.PesquisarDA();
+        }
+    }
+}
