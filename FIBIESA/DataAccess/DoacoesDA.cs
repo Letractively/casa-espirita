2e50d89f3373dbf70c2efc379497f96f68b9009c
viniciusbalbinot91 <viniciusbalbinot91@gmail.com>
2012-12-26 17:10:31 +0000

    Criado classes das tabela Doaçoes

---
+++ FIBIESA/DataAccess/DoacoesDA.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataObjects;
+
+namespace DataAccess
+{
+    public class DoacoesDA
+    {
+        public bool InserirDA(Doacoes doa)
+        {
+            return true;
+        }
+
+        public bool EditarDA(Doacoes doa)
+        {
+            return true;
+        }
+
+        public bool ExcluirDA(Doacoes doa)
+        {
+            return true;
+        }
+
+        public List<Doacoes> PesquisarDA()
+        {
+            List<Doacoes> doacoes = new List<Doacoes>();
+            return doacoes;
+        }
+    }
+}
---
+++ FIBIESA/DataObjects/Doacoes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataObjects
+{
+    public class Doacoes
+    {
+        private Int32 _id;
+        public Int32 Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private Int32 _pessoaId;
+        public Int32 PessoaId
+        {
+            get { return _pessoaId; }
+            set { _pessoaId = value; }
+        }
+
+        private DateTime _data;
+        public DateTime Data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+
+        private float _valor;
+        private float Valor
+        {
+            get { return _valor; }
+            set { _valor = value; }
+        }
+
+        private Int32 _usuarioId;
+        private Int32 UsuarioId
+        {
+            get { return _usuarioId; }
+            set { _usuarioId = value; }
+        }
+    
+    }
+}
