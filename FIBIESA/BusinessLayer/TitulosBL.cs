0cd0ca79277b5242e4d88ca7112be416f45b23b7
boff.everton <boff.everton@gmail.com>
2013-04-29 17:38:10 +0000

    Titulo

--- FIBIESA/BusinessLayer/TitulosBL.cs
+++ FIBIESA/BusinessLayer/TitulosBL.cs
@@ -13,56 +13,46 @@ namespace BusinessLayer
 
         public bool InserirBL(Titulos tit)
         {
-            /*criar as regras de negocio*/
             TitulosDA titDA = new TitulosDA();
-
             return titDA.InserirDA(tit);
         }
 
         public bool EditarBL(Titulos tit)
         {
-            /*criar as regras de negocio*/
             TitulosDA titDA = new TitulosDA();
-
             return titDA.EditarDA(tit);
         }
 
         public bool ExcluirBL(Titulos tit)
         {
-            /*criar as regras de negocio*/
             TitulosDA titDA = new TitulosDA();
-
             return titDA.ExcluirDA(tit);
         }
 
         public List<Titulos> PesquisarBL()
         {
-            /*criar as regras de negocio*/
             TitulosDA titDA = new TitulosDA();
-
             return titDA.PesquisarDA();
         }
 
         public List<Titulos> PesquisarBL(int pes)
         {
             TitulosDA titDA = new TitulosDA();
-
             return titDA.PesquisarDA(pes);
         }
 
         public List<Titulos> PesquisarBL(string campo, string valor)
         {
             TitulosDA titulosDA = new TitulosDA();
-
             return titulosDA.PesquisarDA(campo, valor);
         }
 
-        public override List<Base> Pesquisar(string codDes, string tipo)
+        public override List<Base> Pesquisar(string tipo)
         {
             TitulosDA titDA = new TitulosDA();
-
-            return titDA.Pesquisar(codDes, tipo);
+            return titDA.Pesquisar(tipo);
         }
+
     }
 
 
