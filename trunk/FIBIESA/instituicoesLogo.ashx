3f235e6eb3487405f51ac5e3c3d362f2f62311ac
wracko@gmail.com <wracko@gmail.com@b50c96da-cf07-9db0-1c6e-8329b04c20b5>
2013-05-19 01:16:20 +0000

    Mostra o logo no cadastro de instituicoes

---
+++ trunk/FIBIESA/instituicoesLogo.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="instituicoesLogo.ashx.cs" Class="FIBIESA.instituicoesLogo1" %>
---
+++ trunk/FIBIESA/instituicoesLogo.ashx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BusinessLayer;
+using DataObjects;
+
+namespace FIBIESA
+{
+    /// <summary>
+    /// Carrega o logo da instituicao
+    /// </summary>
+    public class instituicoesLogo : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.Request.QueryString["logoId"] != null)
+            {
+                InstituicoesBL ibl = new InstituicoesBL();
+                int id = Convert.ToInt16(context.Request.QueryString["logoId"]);
+                List<Instituicoes> lista = ibl.PesquisarBL(id);
+                byte[] img = lista[0].InstituicaoLogo.Imagem;
+
+                if (img != null)
+                {
+                    context.Response.ContentType = "image/" + lista[0].InstituicaoLogo.Extensao;
+                    context.Response.OutputStream.Write(img, 0, img.Length);
+                }
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
