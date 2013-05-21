d503554301b10dddab591778e0f7de5320da50a3
boff.everton@gmail.com <boff.everton@gmail.com@b50c96da-cf07-9db0-1c6e-8329b04c20b5>
2013-05-21 13:33:24 +0000

    boleto

--- trunk/FIBIESA/WebForm1.aspx.cs
+++ trunk/FIBIESA/WebForm1.aspx.cs
@@ -33,65 +33,69 @@ namespace FIBIESA
         protected void Button1_Click(object sender, EventArgs e)
         {
 
-            string vencimento = "01/01/2013";
-            String valorBoleto = "110,50";
-            String numeroDocumento = "B20005446";
+            for (int i = 0; i < 3; i++)
+            {
 
-            //cedente
-            String cedente_codigo = "13000";
-            String cedente_nossoNumeroBoleto = "22222222";
-            String cedente_cpfCnpj = "123.456.789-01";
-            String cedente_nome = "PAULO FREIRE - FOUR FREIRES INF.";
-            String cedente_agencia = "1000";
-            String cedente_conta = "22507";
-            String cedente_digitoConta = "6";
+                string vencimento = "01/01/2013";
+                String valorBoleto = "110,50";
+                String numeroDocumento = "B20005446";
 
-
-            //sacado
-            String sacado_cpfCnpj = "000.000.000-00";
-            String sacado_nome = "eu caralho";
-            String sacado_endereco = "xxxxxxxxxxxx";
-            String sacado_bairro = "ffffffffffffff";
-            String sacado_cidade = "caxias";
-            String sacado_cep = "95070200";
-            String sacado_uf = "rs";
-
-            Cedente cedente = new Cedente(cedente_cpfCnpj,
-            cedente_nome,
-            cedente_agencia,
-            cedente_conta,
-            cedente_digitoConta);
-            cedente.Codigo = Convert.ToInt32(cedente_codigo);
-
-            Boleto boleto = new Boleto(Convert.ToDateTime(vencimento), Convert.ToDecimal(valorBoleto), "341", cedente_nossoNumeroBoleto, cedente);
-
-            boleto.NumeroDocumento = numeroDocumento;
-
-            Sacado sacado = new Sacado(sacado_cpfCnpj, sacado_nome);
-            boleto.Sacado = sacado;
-            boleto.Sacado.Endereco.End = sacado_endereco;
-            boleto.Sacado.Endereco.Bairro = sacado_bairro;
-            boleto.Sacado.Endereco.Cidade = sacado_cidade;
-            boleto.Sacado.Endereco.CEP = sacado_cep;
-            boleto.Sacado.Endereco.UF = sacado_uf;
-
-            Instrucao_Banrisul instrucao = new Instrucao_Banrisul(999, 1);
-            Instrucao_Banrisul item1 = new Instrucao_Banrisul(9, 5);
-            Instrucao_Banrisul item2 = new Instrucao_Banrisul(81, 10);
-            boleto.Instrucoes.Add(instrucao);
-            boleto.Instrucoes.Add(item1);
-            boleto.Instrucoes.Add(item2);
-
-            BoletoBancario boleto_bancario = new BoletoBancario();
-            boleto_bancario.CodigoBanco = 041;
-            boleto_bancario.Boleto = boleto;
-            boleto_bancario.MostrarCodigoCarteira = true;
-            boleto_bancario.Boleto.Valida();
-
-            boleto_bancario.MostrarComprovanteEntrega = true;
+                //cedente
+                String cedente_codigo = "13000";
+                String cedente_nossoNumeroBoleto = "22222222";
+                String cedente_cpfCnpj = "123.456.789-01";
+                String cedente_nome = "PAULO FREIRE - FOUR FREIRES INF.";
+                String cedente_agencia = "1000";
+                String cedente_conta = "22507";
+                String cedente_digitoConta = "6";
 
 
-            this.Panel1.Controls.Add(boleto_bancario);
+                //sacado
+                String sacado_cpfCnpj = "000.000.000-00";
+                String sacado_nome = "eu caralho";
+                String sacado_endereco = "xxxxxxxxxxxx";
+                String sacado_bairro = "ffffffffffffff";
+                String sacado_cidade = "caxias";
+                String sacado_cep = "95070200";
+                String sacado_uf = "rs";
+
+                Cedente cedente = new Cedente(cedente_cpfCnpj,
+                cedente_nome,
+                cedente_agencia,
+                cedente_conta,
+                cedente_digitoConta);
+                cedente.Codigo = Convert.ToInt32(cedente_codigo);
+
+                Boleto boleto = new Boleto(Convert.ToDateTime(vencimento), Convert.ToDecimal(valorBoleto), "341", cedente_nossoNumeroBoleto, cedente);
+
+                boleto.NumeroDocumento = numeroDocumento;
+
+                Sacado sacado = new Sacado(sacado_cpfCnpj, sacado_nome);
+                boleto.Sacado = sacado;
+                boleto.Sacado.Endereco.End = sacado_endereco;
+                boleto.Sacado.Endereco.Bairro = sacado_bairro;
+                boleto.Sacado.Endereco.Cidade = sacado_cidade;
+                boleto.Sacado.Endereco.CEP = sacado_cep;
+                boleto.Sacado.Endereco.UF = sacado_uf;
+
+                Instrucao_Banrisul instrucao = new Instrucao_Banrisul(999, 1);
+                Instrucao_Banrisul item1 = new Instrucao_Banrisul(9, 5);
+                Instrucao_Banrisul item2 = new Instrucao_Banrisul(81, 10);
+                boleto.Instrucoes.Add(instrucao);
+                boleto.Instrucoes.Add(item1);
+                boleto.Instrucoes.Add(item2);
+
+                BoletoBancario boleto_bancario = new BoletoBancario();
+                boleto_bancario.CodigoBanco = 041;
+                boleto_bancario.Boleto = boleto;
+                boleto_bancario.MostrarCodigoCarteira = true;
+                boleto_bancario.Boleto.Valida();
+
+                boleto_bancario.MostrarComprovanteEntrega = true;
+
+
+                this.Panel1.Controls.Add(boleto_bancario);
+            }
         }
     }
 }
