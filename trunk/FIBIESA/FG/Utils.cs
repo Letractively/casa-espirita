b04a09a3dcaa6a17398eb700ae007d38213d07f0
boff.everton@gmail.com <boff.everton@gmail.com@b50c96da-cf07-9db0-1c6e-8329b04c20b5>
2013-06-18 23:41:44 +0000

    cpf cnpj

--- trunk/FIBIESA/FG/Utils.cs
+++ trunk/FIBIESA/FG/Utils.cs
@@ -324,6 +324,97 @@ namespace FG
             pCNPJ = pCNPJ.Replace("/", "");
             return pCNPJ;
         }
+
+        public static bool ValidaCPF(string cpf)
+        {
+            int[] multiplic1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplic2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf;
+            string digit;
+            int sum;
+            int remainder;
+
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "").Replace(",", "");
+
+            if (cpf.Length != 11)
+                return false;
+
+            tempCpf = cpf.Substring(0, 9);
+            sum = 0;
+            for (int i = 0; i < 9; i++)
+                sum += int.Parse(tempCpf[i].ToString()) * multiplic1[i];
+
+            remainder = sum % 11;
+            if (remainder < 2)
+                remainder = 0;
+            else
+                remainder = 11 - remainder;
+
+            digit = remainder.ToString();
+
+            tempCpf = tempCpf + digit;
+
+            sum = 0;
+            for (int i = 0; i < 10; i++)
+                sum += int.Parse(tempCpf[i].ToString()) * multiplic2[i];
+
+            remainder = sum % 11;
+            if (remainder < 2)
+                remainder = 0;
+            else
+                remainder = 11 - remainder;
+
+            digit = digit + remainder.ToString();
+
+            return cpf.EndsWith(digit);
+        }
+
+        public static bool ValidaCNPJ(string cnpj)
+        {
+            int[] multiplic1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplic2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int sum;
+            int remainder;
+            string digit;
+            string tempCnpj;
+
+            cnpj = cnpj.Trim();
+            cnpj = cnpj.Replace(".", "").Replace("-", "").Replace("/", "").Replace(",", "");
+
+            if (cnpj.Length != 14)
+                return false;
+
+            tempCnpj = cnpj.Substring(0, 12);
+
+            sum = 0;
+            for (int i = 0; i < 12; i++)
+                sum += int.Parse(tempCnpj[i].ToString()) * multiplic1[i];
+
+            remainder = (sum % 11);
+            if (remainder < 2)
+                remainder = 0;
+            else
+                remainder = 11 - remainder;
+
+            digit = remainder.ToString();
+
+            tempCnpj = tempCnpj + digit;
+            sum = 0;
+            for (int i = 0; i < 13; i++)
+                sum += int.Parse(tempCnpj[i].ToString()) * multiplic2[i];
+
+            remainder = (sum % 11);
+            if (remainder < 2)
+                remainder = 0;
+            else
+                remainder = 11 - remainder;
+
+            digit = digit + remainder.ToString();
+
+            return cnpj.EndsWith(digit);
+
+        }
     }
        
 }
